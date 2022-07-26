641b3d35c147d6221209af34616d616efe81233f
myjs999 <35965093+myjs999@users.noreply.github.com>
2022-07-26 15:25:58 +0800

    now you can use if

--- splendid_compiler.cs
+++ splendid_compiler.cs
@@ -12,6 +12,47 @@ namespace Splendid__
         {
 
         }
+        public string FindBracketing(string s, char bra, char ket)
+        {
+            string ret = "";
+            char inString = ' ';
+            int bed = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c == '\\')
+                {
+                    i++;
+                    continue;
+                }
+                if (inString != ' ')
+                {
+                    if (c == '\"' && inString == '\"')
+                    {
+                        inString = ' ';
+                    }
+                    else if (c == '\'' && inString == '\'')
+                    {
+                        inString = ' ';
+                    }
+                }
+                else
+                {
+                    if (c == bra) ++bed;
+                    if (bed > 0)
+                    {
+                        ret += c;
+                    }
+                    if (c == ket) --bed;
+                    if (bed == 0 && ret != "") return ret;
+                }
+            }
+            return ret;
+        }
+        public class StringUtilities
+        {
+            
+        }
         public string code = "";
         public string output = "";
         const string ENDL = "\r\n";
@@ -112,7 +153,7 @@ namespace Splendid__
                 }
                 if(inString == 0 && inMac == 0)
                 {
-                    if(c == ';' || c == '{' || c == '}')
+                    if(c == ';' || c == '{' || c == '}' || c == ':')
                     {
                         while (curHandle.Length >= 2 && curHandle[curHandle.Length-2] == ' ')
                         {
@@ -153,9 +194,41 @@ namespace Splendid__
                 else if (typ == 0) return "undefined";
                 else return "";
             }
+            public bool AsBool()
+            {
+                if (typ == 1) return a != 0;
+                if (typ == 2) return s != "";
+                return false;
+            }
+            public void Plus(VarType o)
+            {
+                if(typ == 1)
+                {
+                    if (o.typ == 1)
+                    {
+                        a += o.a;
+                    }
+                    else if (o.typ == 2)
+                    {
+                        s = a.ToString();
+                        typ = 2;
+                        s += o.s;
+                    }
+                }else if(typ == 2)
+                {
+                    if (o.typ == 1)
+                    {
+                        s += o.a.ToString();
+                    }
+                    else if (o.typ == 2)
+                    {
+                        s += o.s;
+                    }
+                }
+            }
         }
-        Dictionary<string, VarType> vars = new Dictionary<string, VarType>();
-        VarType ParseRH(string rh)
+        Dictionary<string, VarType> heapVars = new Dictionary<string, VarType>();
+        VarType ParseRH(string rh, Dictionary<string, VarType> vars)
         {
             if (rh[0] == '\"' && rh.Last() == '\"')
             {
@@ -174,33 +247,88 @@ namespace Splendid__
             {
                 return vars[rh];
             }
+
             return new VarType();
         }
         void PushOutput(string s)
         {
             output += s;
         }
-        void RunAt(int hi)
+        int FindEndHandleFromBegin(int start)
+        {
+            int bed = 0;
+            for (; ; )
+            {
+                if (handles[start].Last() == '{') ++bed;
+                if (handles[start].Last() == '}') --bed;
+                if (bed == 0) return start;
+                ++start;
+                if (start >= handles.Count) return -1;
+            }
+        }
+        class Stringwm // string with mark
+        {
+            public string s = "";
+            public int mark = 0;
+            public Stringwm(string s)
+            {
+                this.s = s;
+            }
+        }
+        int RunAt(int hi, Dictionary<string, VarType> vars)
         {
             string ch = handles[hi];
+            if(ch == "{")
+            {
+                int end = FindEndHandleFromBegin(hi);
+                ++hi;
+                for(; ; )
+                {
+                    if (hi == end) break;
+                    hi = RunAt(hi, vars);
+                }
+                return end + 1;
+            }
             ch = ch.Remove(ch.Length - 1);
             if(ch == "helloworld")
             {
                 PushOutput("hello, world!");
-                return;
+                return hi+1;
             }
             string firstWord = GetFirstWord(ch);
             string s2 = PreTrimmed(ch.Substring(firstWord.Length));
             if (firstWord == "print")
             {
-                PushOutput(ParseRH(s2).AsString());
-                return;
+                PushOutput(ParseRH(s2, vars).AsString());
+                return hi+1;
+            }else if(firstWord == "if")
+            {
+                string param = FindBracketing(s2, '(', ')');
+                bool execute = ParseRH(param.Substring(1, param.Length - 2), vars).AsBool();
+                if (execute)
+                {
+                    return hi + 1;
+                    //PushOutput("yesdo");
+                }
+                else
+                {
+                    return FindEndHandleFromBegin(hi + 1)+1;
+                    //PushOutput("nodo");
+                }
             }
             string firstSign = GetFirstSign(s2);
+            string s3 = PreTrimmed(s2.Substring(firstSign.Length));
             if (firstSign == "=")
             {
-                vars[firstWord] = ParseRH(PreTrimmed(s2.Substring(firstSign.Length)));
+                vars[firstWord] = ParseRH(s3, vars);
+            }else if(firstSign == "+=")
+            {
+                if (vars.ContainsKey(firstWord))
+                    vars[firstWord].Plus(ParseRH(s3, vars));
+                else
+                    vars[firstWord] = ParseRH(s3, vars);
             }
+            return hi + 1;
         }
         public void Run()
         {
@@ -211,10 +339,17 @@ namespace Splendid__
                 output += handles[i] + ENDL;
             }
             output += "--------\r\n";
-            for (int i = 0; i < handles.Count; i++)
+            int hi = 0;
+            for(; ;)
             {
-                RunAt(i);
+                hi = RunAt(hi, heapVars);
+                if (hi >= handles.Count || hi < 0) break;
             }
+            //for (int i = 0; i < handles.Count; i++)
+            //{
+            //    RunAt(i);
+            //    break;
+            //}
             //if(handles.Count > 0)
             //    RunAt(0);
             
